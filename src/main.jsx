83b8247e6ee1e872b8ecdeda8933b5daff180eac
nyagajr <muchokijnrbenson@gmail.com>
2024-07-02 16:16:14 +0200

    New repo

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import TodoList from './App.jsx'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <TodoList />
+  </React.StrictMode>,
+)
